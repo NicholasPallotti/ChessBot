39902cb636d49cf231297a20ecd700350f2dd012
Nicholas Pallotti <tcnp1996@gmail.com>
2022-12-13 13:39:54 -0700

    added method to creat players' pieces

---
+++ Pieces.py
@@ -0,0 +1,41 @@
+def MakePieces():
+    pieces = [Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Knight, Knight, Rook, Rook, Bishop, Bishop, Queen, King]
+
+class Pawn:
+    def __init__(self):
+        pass
+
+    def Move():
+        pass
+
+class Knight:
+    def __init__(self):
+        pass
+
+    def move():
+        pass
+
+class Rook:
+    def __init__(self):
+        pass
+
+class Bishop:
+    def __init__(self):
+        pass
+
+    def move():
+        pass
+
+class Queen:
+    def __init__(self):
+        pass
+
+    def move():
+        pass
+
+class King:
+    def __init__(self):
+        pass
+
+    def move():
+        pass
--- player.py
+++ player.py
@@ -1,5 +1,9 @@
+import Peices as p
+
 class Player:
     def __init__(self, color):
         self.color = color
 
+        pieces = p.MakePieces()
+        
         #create pieces
